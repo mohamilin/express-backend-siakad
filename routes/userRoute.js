b0523cb7ae337edc62e1c828dec579bc7a8601bc
mohamilin <amilinmoh99@gmail.com>
2021-01-03 14:16:34 +0700

    better with me

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,31 +4,31 @@ const { register, login } = require("../controllers/authController");
 const { verifyRegister } = require("../middleware");
 
 // versi 1
-// router.post(
-//   "/register",
-//   [verifyRegister.checkUserNameOrEmail, verifyRegister.checkRole],
-//   register
-// );
+router.post(
+  "/register",
+  [verifyRegister.checkUserNameOrEmail, verifyRegister.checkRole],
+  register
+);
 
-// router.post("/login", login);
-// module.exports = router;
+router.post("/login", login);
+module.exports = router;
 
 // versi 2
-module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-      next();
-    });
+// module.exports = function (app) {
+//     app.use(function (req, res, next) {
+//         res.header(
+//             "Access-Control-Allow-Headers",
+//             "x-access-token, Origin, Content-Type, Accept"
+//         );
+//       next();
+//     });
   
-    app.post(
-      "/api/register",
-      [verifyRegister.checkUserNameOrEmail, verifyRegister.checkRole],
-      register
-    );
+//     app.post(
+//       "/api/register",
+//       [verifyRegister.checkUserNameOrEmail, verifyRegister.checkRole],
+//       register
+//     );
   
-    app.post("/api/login", login);
-  };
+//     app.post("/api/login", login);
+//   };
   
